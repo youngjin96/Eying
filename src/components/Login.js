dc20aad23ad1252a47324a555843bc87fa08a026
kodonghun <kdhkdh0101@gmail.com>
2022-03-14 00:22:59 +0900

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,143 @@
-const Login = () => {
-    return(
-        <div>
-            Login Page
-        </div>
-    )
-}
+import React, { useState } from 'react';
+import { auth } from "./Fbase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from '@mui/icons-material';
 
-export default Login
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
+    const onChange = (event) => {
+        const { target: { name, value } } = event;
+        if (name === "email") {
+            setEmail(value)
+        } else if (name === "password") {
+            setPassword(value)
+        }
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            let data;
+            if (newAccount) {
+                // create account
+                data = await createUserWithEmailAndPassword(auth, email, password);
+            } else {
+                // log in
+                data = await signInWithEmailAndPassword(auth, email, password);
+            }
+            console.log(data)
+        } catch (error) {
+            console.log(error);
+            setError(error.message)
+        }
+    }
+
+    const signInWithGoogle = () => {
+
+        const provider = new GoogleAuthProvider();
+        provider.setCustomParameters({ prompt: 'select_account' });
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result);
+            }).catch((error) => {
+                console.log(error);
+            })
+
+    }
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
+    const theme = createTheme();
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+
+                    <Typography component="h1" variant="h5">
+                        Sign In
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={onSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    value={email}
+                                    onChange={onChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    value={password}
+                                    onChange={onChange}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            value={newAccount ? "Create Account" : "Sign In"}
+                            onSubmit={onSubmit}
+                        >
+                            <Link to="/home">
+                                Sign In
+                            </Link>
+                        </Button>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={toggleAccount}
+                        >{newAccount ? "Sign in" : "Create Account"}
+                        </Button>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={signInWithGoogle}
+                        >
+                            google
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+};
+export default Login;
