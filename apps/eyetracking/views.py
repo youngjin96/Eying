8cee21b733a9ff63277bfd5518eaf66a23ed018f
jiyoony <https://github.com/jiyoony/webclient.git>
2022-04-18 18:09:50 +0900

    eyetracking view 수정

--- apps/eyetracking/views.py
+++ apps/eyetracking/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 from user.models import User
 from pdf.models import PDFModel
 from .models import Eyetracking
+from .serializers import EyetrackingSerializer
 
 def index(request):
     return HttpResponse("hello")
@@ -13,10 +16,13 @@ class EyetrackList(APIView):
     def post(self,request):
         print(request.data)
         # try :
-        eyetrackdatas = Eyetracking(user_id = User.objects.get(pk=1), owner_id = User.objects.get(pk=1), page_num = request.data['page_num'], 
-                                    pdf_fk = PDFModel.objects.get(pk=1), rating_time= request.data['rating_time'],coordinate= request.data['coordinate'])
+
+        eyetrackdatas = Eyetracking(user_id = User.objects.get(pk=request.data['user_id']), owner_id = User.objects.get(pk=request.data['owner_id']), page_num = request.data['page_number'], 
+                                    pdf_fk = PDFModel.objects.get(pk=request.data['pdf_id']), rating_time= request.data['rating_time'],coordinate= request.data['coordinate'])
         eyetrackdatas.save()
-        return HttpResponse(200)
+
+        serializer = EyetrackingSerializer(eyetrackdatas, many=False)
+        return Response(serializer.data, status = status.HTTP_200_OK)
     
     def put(self,request):
         pass
