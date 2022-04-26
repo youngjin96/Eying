f0d7838179d4d41a94241925fe4b96de8eeca5c7
Kim-Changgyu <kimc980106@naver.com>
2022-04-26 18:45:59 +0900

    사용자 REST API (UPDATE, DELETE) 추가 및 POST 업데이트

--- apps/user/models.py
+++ apps/user/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    age = models.IntegerField(default=0)                        # 나이
-    gender = models.CharField(default="None", max_length=10)    # 성별
-    job_field = models.CharField(default="None", max_length=50) # 분야
-    job = models.CharField(default="None", max_length=50)       # 직종
-    position = models.CharField(default="None", max_length=50)  # 직책
-    credit = models.IntegerField(default=0, blank=True)         # 크레딧 (포인트)
-    card = models.ImageField(default=None)                      # 명함 이미지
+    age = models.IntegerField(default=0)                                    # 나이
+    gender = models.CharField(default=None, null=True, max_length=10)       # 성별
+    job_field = models.CharField(default=None, null=True, max_length=50)    # 분야
+    job = models.CharField(default=None, null=True, max_length=50)          # 직종
+    position = models.CharField(default=None, null=True, max_length=50)     # 직책
+    credit = models.IntegerField(default=0, blank=True)                     # 크레딧 (포인트)
+    card = models.ImageField(default=None)                                  # 명함 이미지
     
     def __str__(self):
         return self.username
--- apps/user/views.py
+++ apps/user/views.py
@@ -19,7 +19,7 @@ class UserAPI(APIView):
             user_password = request.GET.get("password", None)
             
             if not user_email or not user_password:
-                error_message = "아이디 / 비밀번호 입력 오류입니다."
+                error_message = "아이디 / 패스워드 입력 오류입니다."
                 raise
             
             queryset = User.objects.get(Q(email=user_email))
@@ -28,7 +28,7 @@ class UserAPI(APIView):
                 serializer = UserSerializer(queryset, many=False)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             else:
-                error_message = "비밀번호가 올바르지 않습니다."
+                error_message = "패스워드가 올바르지 않습니다."
                 raise
         except Exception as e:
             print(e)
@@ -38,21 +38,27 @@ class UserAPI(APIView):
     def post(self, request):
         error_message = "알 수 없는 오류가 발생했습니다."
         try:
+            # 중복 이메일 체크
             if User.objects.filter(email=request.data["email"]):
                 error_message = "이미 존재하는 이메일입니다."
                 raise
             
+            # 비밀번호 재검증
+            if request.POST.get("password") == None:
+                error_message = "패스워드가 입력되지 않았습니다."
+                raise
+                
             user = User(
-                username=request.data["username"],
-                password=make_password(str(request.data["password"])),
-                email=request.data["email"],
-                age=request.data["age"],
-                gender=request.data["gender"],
-                job=request.data["job"],
-                job_field=request.data["job_field"],
-                position=request.data["position"],
-                credit=request.data["credit"],
-                card=request.data["card"],
+                username=request.POST.get("username"),
+                password=make_password(request.POST.get("password")),
+                email=request.POST.get("email"),
+                age=request.POST.get("age", 0),
+                gender=request.POST.get("gender"),
+                job=request.POST.get("job"),
+                job_field=request.POST.get("job_field"),
+                position=request.POST.get("position"),
+                credit=request.POST.get("credit", 0),
+                card=request.FILES.get("card"),
             )
             user.save()
             
@@ -63,13 +69,63 @@ class UserAPI(APIView):
             return Response({'error_message': error_message}, status=status.HTTP_400_BAD_REQUEST)
         
     @TIME_MEASURE
-    def delete(self, request):
+    def put(self, request):
         try:
-            print("DELETE 요청")
-            return Response({}, status=status.HTTP_200_OK)
+            user_email = request.POST.get("email")
+            operator = request.POST.get("operator")
+            credit = int(request.POST.get("credit"))
+            
+            # 요청 데이터 누락 처리
+            if user_email == None or operator == None or credit == None:
+                error_message = "이메일 | 연산자 | 크레딧이 입력되지 않았습니다."
+                raise
+            elif operator not in ["+", "-", "="]:
+                error_message = "연산자가 올바르지 않습니다."
+                raise
+            
+            # 사용자 검증
+            user = User.objects.get(email=user_email)
+            if not user:
+                error_message = "해당 이메일의 사용자가 존재하지 않습니다."
+                raise
+            
+            # 업데이트
+            if operator == "+":
+                user.credit = user.credit + credit
+            elif operator == "-":
+                user.credit = user.credit - credit
+            elif operator == "=":
+                user.credit = credit
+            user.save(update_fields=['credit'])
+            
+            serializer = UserSerializer(user, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             print(e)
-            return Response({'error_message': e}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error_message': error_message}, status=status.HTTP_400_BAD_REQUEST)
+        
+    @TIME_MEASURE
+    def delete(self, request):
+        try:
+            # 요청 데이터 누락 처리
+            user_email = request.POST.get("email")
+            if user_email == None:
+                error_message = "이메일이 입력되지 않았습니다."
+                raise
+            
+            # 사용자 검증
+            user = User.objects.get(email=user_email)
+            if not user:
+                error_message = "해당 이메일의 사용자가 존재하지 않습니다."
+                raise
+            
+            user.delete()
+            
+            serializer = UserSerializer(user, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return Response({'error_message': error_message}, status=status.HTTP_400_BAD_REQUEST)
                 
 class UserSearchAPI(APIView):
     @TIME_MEASURE
