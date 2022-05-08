9b5325fc80f10d19b78a45b4d2d3ab8c4ac74390
jiyoony <https://github.com/jiyoony/webclient.git>
2022-05-08 21:50:34 +0900

    시각화 처리

--- apps/eyetracking/views.py
+++ apps/eyetracking/views.py
@@ -1,33 +1,25 @@
-from django.shortcuts import render
-from numpy import eye
 from rest_framework.views import APIView
-from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
 from user.models import User
 from pdf.models import PDFModel
 from .models import Eyetracking
 from .serializers import EyetrackingSerializer,EyetrackingUserList,Userlist,CoordinateSerializer
+from pdf.serializers import PDFSerializer
 from django.db.models import F
 from apps.settings import STATIC_URL
 from .heatmap import Heatmapper
-from PIL import Image
-from PIL import ImageDraw
+
+from PIL import Image,ImageDraw
 import matplotlib.pyplot as plt
-# import urllib.request # 웹 상의 이미지를 다운로드 없이 사용
+from apps.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_CUSTOME_DOMAIN, AWS_STORAGE_BUCKET_NAME
+import boto3
 from io import BytesIO
-from urllib import request
 import datetime
 import urllib.request
 import ast
-from pdf.serializers import PDFSerializer
-
-        # if queryDict['visual_type'] == 'flow':
-        # # 이미지 위에 히트맵 그리기
-        #     heatmap = heatmapper.heatmap_on_img(coordinate,_img)
-        #     heatmap.save('image/'+img_path+'png')
-        #     queryDict['visual_img'] = heatmap
-        #     serializer = EyetrakcingSerializer(queryset, many=True)
+from apps.decorator import TIME_MEASURE
+import asyncio # 비동기 처리
 heatmapper = Heatmapper(
     point_diameter=100,  # the size of each point to be drawn
     point_strength=1,  # the strength, between 0 and 1, of each point to be drawn
@@ -39,11 +31,72 @@ heatmapper = Heatmapper(
                            # Pillow used by default, 'PySide' option available if installed
 )
 
+s3r = boto3.resource('s3', aws_access_key_id = AWS_ACCESS_KEY_ID, aws_secret_access_key= AWS_SECRET_ACCESS_KEY)
 
 class EyetrackList(APIView):
+    async def visualization(self,user_id,pdf_id,page_num,owner_id,coordinate):
+        global heatmapper
+        global s3r
+        print("시각화 처리")
+        try:
+            save_flow_path = "media/public/user_{0}/pdf/{1}/{2}/images/{3}.jpg".format(owner_id,pdf_id,"flow",page_num)
+            save_distribution_path = "media/public/user_{0}/pdf/{1}/{2}/images/{3}.jpg".format(owner_id,pdf_id,"distribution",page_num)
+            # 이미지 url
+            img_path = STATIC_URL+"media/public/user_{0}/pdf/{1}/images/{2}.jpg".format(owner_id,pdf_id,page_num)
+            
+            
+            try:
+                web_img = urllib.request.urlopen(img_path).read()
+                _img = Image.open(BytesIO(web_img))
+            except Exception as e:
+                print("이미지 에러",e)
+                return Response({'error_message': "이미지 에러"})
+            
+            coordinate = ast.literal_eval(coordinate)
+
+            try:
+                heatmap = heatmapper.heatmap_on_img(coordinate, _img)
+                heatmap = heatmap.convert("RGB")
+
+                buffer = BytesIO()
+                heatmap.save(buffer,format='jpeg')
+                buffer.seek(0)
+
+                s3r.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(
+                                    Key=save_flow_path, 
+                                    Body=buffer,  
+                                    ContentType='image/jpeg')
+                print("시선분산 처리 완료")
+            except Exception as e:
+                print("히트맵 오류",e)
+                return Response({'error_message': "히트맵 에러"})
+                 
+            try:   
+                draw = ImageDraw.Draw(_img)
+                color = ["#FF0000", "#FF5E00", "#FFBB00", "#FFE400", "#ABF200", "#1DDB16", "#00D8FF", "#0054FF", "#0100FF", "#5F00FF"]
+                for i in range(len(coordinate)-1):
+                    x,y = coordinate[i]
+                    x2,y2 = coordinate[i+1]
+                    draw.line((x,y,x2,y2),fill = color[i//10%10] ,width = 5)
+                
+                buffer = BytesIO()
+                _img.save(buffer, format='jpeg')
+                buffer.seek(0) # 대기
+
+                s3r.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(
+                                    Key=save_distribution_path, 
+                                    Body=buffer,  
+                                    ContentType='image/jpeg')
+                print("flow 처리 완료")
+            except Exception as e:
+                print("flow 처리 에러 ",e)
+                return Response(e, status=status.HTTP_200_OK)
+            
+        except Exception as e: 
+            print(e)
+            return Response({'error_message': "시각화 오류"})
+
     def post(self,request):
-        print(request.data)
-        # try :
         user_email = request.data['user_email']
         owner_email = request.data['owner_email']
         coordinate = request.data['coordinate']
@@ -51,11 +104,13 @@ class EyetrackList(APIView):
         pdf_id = request.data['pdf_id']
         rating_time = request.data['rating_time']
         
+        user_id =  User.objects.get(email=user_email).pk
+        owner_id = User.objects.get(email=owner_email).pk
         # 트래킹 데이터 이어쓰기
-        if Eyetracking.objects.get(pdf_fk=PDFModel.objects.get(pk=pdf_id),
-                                    user_id=User.objects.get(email=user_email),
-                                    page_num=page_num):
-            return self.put(request)
+        # if Eyetracking.objects.get(pdf_fk=PDFModel.objects.get(pk=pdf_id),
+        #                             user_id=User.objects.get(email=user_email),
+        #                             page_num=page_num):
+        #     return self.put(request)
 
         eyetrackdatas = Eyetracking(user_id = User.objects.get(email=user_email),
                                     owner_id = User.objects.get(email=owner_email),
@@ -63,18 +118,26 @@ class EyetrackList(APIView):
                                     pdf_fk = PDFModel.objects.get(pk=pdf_id),
                                     rating_time= rating_time,
                                     coordinate= coordinate)
+                                    
         eyetrackdatas.save()
-
+        try:
+            self.visualization(user_id,pdf_id,page_num,owner_id,coordinate)
+            print("post 시각화 처리")
+        except Exception as e:
+            print(e)
 
         serializer = EyetrackingSerializer(eyetrackdatas, many=False)
         return Response(serializer.data, status = status.HTTP_200_OK)
-        # except:
-        #     return Response({'error_message': "post 오류"}, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request):
+        user_id =  User.objects.get(email=request.data['user_email']).pk
+        owner_id = User.objects.get(email=request.data['owner_email']).pk
+        pdf_id = request.data['pdf_id']
+        page_num = request.data['page_number']
+
         eyetrackdatas = Eyetracking.objects.get(pdf_fk=PDFModel.objects.get(pk=request.data['pdf_id']),
                                                 user_id=User.objects.get(email=request.data['user_email']),
-                                                page_num=request.data['page_number'])
+                                                page_num=page_num)
         
         delta_time = datetime.time.fromisoformat(request.data['rating_time'])
         
@@ -92,9 +155,12 @@ class EyetrackList(APIView):
 
         # 시선 추적 데이터 이어쓰기
         eyetrackdatas.coordinate = eyetrackdatas.coordinate[:-1] + ", " + request.data['coordinate'][1:-1] + "]"
-        
+        coordinate = eyetrackdatas.coordinate
         eyetrackdatas.save()
         
+        #async 비동기 처리 
+        self.visualization(user_id,pdf_id,page_num,owner_id,coordinate)
+
         serializer = EyetrackingSerializer(eyetrackdatas, many=False)
         return Response(serializer.data)
 
@@ -138,86 +204,20 @@ class EyetrackUser(APIView):
             return Response({'error_message': "user list 오류"})
     
         
-class EyetrackVisualization(APIView):    
-    global heatmapper
+class EyetrackVisualization(APIView):   
     def get(self,request): 
-        try:
-            # 해당 유저들에 대한 좌표 선택
-            queryDict = {
-                'user_id' : request.GET.get("user_email"), 
-                'pdf_id' : request.GET.get("pdf_id"), 
-                'page_num' : request.GET.get('page_number'),
-                'visual_type' : request.GET.get('visual_type'),
-                'owner_email' : request.GET.get('owner_email')
-            }
-            owner_pk = User.objects.get(email=queryDict['owner_email']).pk
-            
-            # 이미지 url
-            img_path = STATIC_URL+"media/public/user_{0}/pdf/{1}/images/{2}.jpg".format(owner_pk, queryDict['pdf_id'], queryDict['page_num'])
-            print("img_path",img_path)
-            
-            res = HttpResponse(content_type="image/png")
-            
-            
-                # _img.save(res, "JPEG")
-                # res['Content-Disposition'] = 'attachment; filename="visual.jpg"'
-                
-                # return res
-            try:
-                web_img = urllib.request.urlopen(img_path).read()
-                # print("web_img",web_img)
-                # _img = Image.open((img_path))
-                _img = Image.open(BytesIO(web_img))
-                print("이미지이이이", _img)
-                _img = _img.convert("RGBA")
-                print("이미지2", _img)
-            except Exception as e:
-                print("이미지 에러",e)
-                return Response({'error_message': "이미지 에러"})
-            
-            # Mysql 쿼리 - eyetracking data 가져오기
-            query = Eyetracking.objects.filter(owner_id=owner_pk,page_num=queryDict['page_num'],pdf_fk=queryDict['pdf_id'])[0]
-            print(query)
-            coordinate = ast.literal_eval(query.coordinate)
-            try:
-                if queryDict['visual_type'] == 'distribution':
-                # 이미지 위에 히트맵 그리기
-                    try:
-                        heatmap = heatmapper.heatmap_on_img(coordinate, _img)
-                        print(heatmap)     
-                        heatmap.save(res, "PNG")
-                        res['Content-Disposition'] = 'attachment; filename="visual.png"'
-                        return res              
-                    except Exception as e:
-                        print("히트맵 오류",e)
-                        return Response({'error_message': "히트맵 에러"})
-                    # return Response({'hh':heatmap}, status=status.HTTP_200_OK)
-            except Exception as e:
-                print("에러명",e)
-                return Response(e, status=status.HTTP_200_OK)
-            # elif queryDict['visual_type'] == 'flow':
-            # # 시각흐름
-            #     draw = ImageDraw.Draw(_img)
-            #     color = ["#FF0000", "#FF5E00", "#FFBB00", "#FFE400", "#ABF200", "#1DDB16", "#00D8FF", "#0054FF", "#0100FF", "#5F00FF"]
-            #     for i in range(len(coordinate)-1):
-            #         x,y = coordinate[i]
-            #         x2,y2 = coordinate[i+1]
-            #         draw.line((x,y,x2,y2),fill = color[i//10%10] ,width = 5)
-            #     _img.save("image/ex1heatmap"+'flow.png')
-            #     queryDict['visual_img'] = _img
-                
-            #     serializer = PDFSerializer(queryset, many=True)
-            #     return Response(serializer.data, status=status.HTTP_200_OK)
+        queryDict = {
+            "owner_email" : request.GET.get("owner_email"),
+            'pdf_id' : request.GET.get('pdf_id'),
+            'user_email' : request.GET.get('user_email'),
+            'visual_type' : request.GET.get('visual_type'),
+        }
+        owner_id =  User.objects.get(email=queryDict['owner_email']).pk
+        image_len = PDFModel.objects.get(pk=queryDict['pdf_id']).img_length
+        img_path = STATIC_URL+"media/public/user_{0}/pdf/{1}/{2}/images/".format(owner_id,queryDict['pdf_id'],queryDict['visual_type'])
+        # img_path = STATIC_URL+"media/public/user_{0}/pdf/{1}/{2}/images/{3}.jpg".format(owner_id,queryDict['pdf_id'],queryDict['visual_type'],queryDict['page_n"  
+        # 
+        return Response({'visual_img': img_path,'img_len' : image_len},status = status.HTTP_200_OK)
 
-            # else:
-            #     return Response({'error_message': "visual type error"}, status=status.HTTP_400_BAD_REQUEST)
-            # return Response({'query_corr':query.coordinate})
-        except Exception as e: 
-        #     # print("해당하는 eyetrakcing 객체가 없습니다.")
-            print(e)
-            return Response({'error_message': "해당하는 eyetracking 객체가 없습니다."})
-
-            # coordinate = query.coordinate
-            # return Response(serializer.data, status=status.HTTP_200_OK)
             
 
