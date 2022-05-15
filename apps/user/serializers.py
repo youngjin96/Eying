5884eb32b338d2b9914f7fc6c73c83bd85fe2c64
Kim-Changgyu <kimc980106@naver.com>
2022-05-16 00:20:30 +0900

    사용자 파일 업로드 개수 추가
    DB에 저장하지 않고 DRF에서 계산 후 반환

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import User
+from pdf.models import PDFModel
 
-class UserSerializer(serializers.ModelSerializer) :
+from drf_yasg.utils import swagger_serializer_method
+class UserSerializer(serializers.ModelSerializer):
+    upload_count = serializers.SerializerMethodField()
+    @swagger_serializer_method(serializer_or_field=serializers.IntegerField)
+    def get_upload_count(self, obj):
+        return len(PDFModel.objects.filter(user=obj))
+    
     class Meta:
         model = User
         # fields = '__all__'    # 전체 필드 직렬화
